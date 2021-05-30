75d4ca3fa976436ed7c6c6ea6fc6413574895878
Prasidh Aggarwal <prasaggarwal@deloitte.com>
2021-05-30 22:47:17 +0530

    Changed recursion function to iterative

--- worker/index.js
+++ worker/index.js
@@ -11,8 +11,19 @@ const sub = redisClient.duplicate();
 
 // Actual fibonacci function
 function fib(index) {
-  if (index < 2) return 1;
-  return fib(index - 1) + fib(index - 2);
+  if (index == 0) return 0;
+  if (index == 1) return 1;
+
+  var prevPrev = 0;
+  var prev = 1;
+  var result = 0;
+
+  for (let i = 2; i <= index; i++) {
+    result = prev + prevPrev;
+    prevPrev = prev;
+    prev = result;
+  }
+  return result;
 }
 
 // Watch redis for any new value put into it
